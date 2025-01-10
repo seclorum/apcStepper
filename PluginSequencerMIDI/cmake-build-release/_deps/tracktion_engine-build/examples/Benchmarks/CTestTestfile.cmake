084c6b61c596cbdf6cd20431281d3f5ceeb4af01
Jay Vaughan <seclorum@mac.com>
2025-01-10 20:18:48 +0100

    Fix CMakeLists.txt to use correct deployment target

--- PluginSequencerMIDI/cmake-build-release/_deps/tracktion_engine-build/examples/Benchmarks/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tompeakwalcher/Documents/develop/apcStepper/modules/tracktion_engine/examples/Benchmarks
-# Build directory: /Users/tompeakwalcher/Documents/develop/apcStepper/PluginSequencerMIDI/cmake-build-release/_deps/tracktion_engine-build/examples/Benchmarks
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Benchmarks "/Users/tompeakwalcher/Documents/develop/apcStepper/PluginSequencerMIDI/cmake-build-release/_deps/tracktion_engine-build/examples/Benchmarks/Benchmarks_artefacts/Release/Benchmarks")
-set_tests_properties(Benchmarks PROPERTIES  _BACKTRACE_TRIPLES "/Users/tompeakwalcher/Documents/develop/apcStepper/modules/tracktion_engine/examples/Benchmarks/CMakeLists.txt;113;add_test;/Users/tompeakwalcher/Documents/develop/apcStepper/modules/tracktion_engine/examples/Benchmarks/CMakeLists.txt;0;")
