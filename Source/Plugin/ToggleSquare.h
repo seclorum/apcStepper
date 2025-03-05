1550c82c8a7318571556889d8821f1b364e132dc
Tom Peak Walcher <tompeakwalcher@Toms-MacBook-Pro.local>
2025-03-05 22:55:33 +0100

    GUI in right lanes

---
+++ Source/Plugin/ToggleSquare.h
@@ -0,0 +1,39 @@
+//
+// Created by Tom Peak Walcher on 05.03.25.
+//
+
+#ifndef TOGGLESQUARE_H
+#define TOGGLESQUARE_H
+
+
+class ToggleSquare : public juce::TextButton {
+public:
+    ToggleSquare(juce::Colour initialColour, juce::Colour toggleColour, const juce::Image &image)
+        : juce::TextButton(""), initialColour(initialColour), toggleColour(toggleColour), buttonImage(image) {
+        setOpaque(true);
+        setColour(juce::TextButton::buttonColourId, initialColour);
+        setClickingTogglesState(true);
+    }
+
+    void paintButton(juce::Graphics &g, bool isMouseOverButton, bool isButtonDown) override {
+        g.fillAll(findColour(juce::TextButton::buttonColourId));
+        if (!buttonImage.isNull()) {
+            g.drawImageWithin(buttonImage, 0, 0, getWidth(), getHeight(), juce::RectanglePlacement::stretchToFit, 1.0f);
+        }
+        isMouseOverButton ? g.setColour(juce::Colours::white) : g.setColour(juce::Colours::darkgrey);
+        g.drawRect(getLocalBounds(), 2);
+    }
+
+    void clicked() override {
+        setColour(juce::TextButton::buttonColourId, getToggleState() ? toggleColour : initialColour);
+        repaint();
+    }
+
+private:
+    juce::Colour initialColour;
+    juce::Colour toggleColour;
+    juce::Image buttonImage;
+};
+
+
+#endif //TOGGLESQUARE_H
