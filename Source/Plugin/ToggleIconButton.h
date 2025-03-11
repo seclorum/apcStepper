6a91954360c934291983dd2861e8de36f0a9bf6f
Tom Peak Walcher <tompeakwalcher@Toms-MacBook-Pro.local>
2025-03-11 06:26:39 +0100

    icons-buttons better

--- Source/Plugin/ToggleIconButton.h
+++ Source/Plugin/ToggleIconButton.h
@@ -22,7 +22,7 @@ public:
 
 
     void paintOverChildren(juce::Graphics &g) override {
-        auto iconImageArea = getLocalBounds().toFloat();
+        auto iconImageArea = getLocalBounds().reduced(getHeight()/7).toFloat();
         if (iconImage && iconImageSize) {
             auto drawAble = juce::Drawable::createFromImageData(iconImage, iconImageSize);
             drawAble->drawWithin(g, iconImageArea, juce::RectanglePlacement::xRight, 1.f);
