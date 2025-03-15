f7b9a98fa931960cf405ae285b9f7b62e752d01d
Jay Vaughan <seclorum@mac.com>
2025-03-15 15:35:31 +0100

    Refactor apcToggleButtonData -> apcToggleParameterButton

---
+++ Source/Plugin/apcToggleParameterButton.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <juce_audio_processors/juce_audio_processors.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_gui_extra/juce_gui_extra.h>
+
+#include <BinaryData.h>
+
+#include "apcStepperMainProcessor.h"
+
+//#include "apcStepperMainEditor.h"
+//#include "ShiftToggleButton.h"
+//#include "apcToggleButton.h"
+//#include "ToggleIconButton.h"
+
+class apcToggleParameterButton : public juce::TextButton {
+public:
+	apcToggleParameterButton(juce::String buttonName, juce::Colour initialColour, juce::Colour toggleColour, apcStepperMainProcessor& p)
+			: juce::TextButton(""), buttonName(buttonName), initialColour(initialColour),
+			  toggleColour(toggleColour), processor(p), isToggled(false)
+	{
+		setClickingTogglesState(true); // Enables automatic toggling
+		setColour(juce::TextButton::buttonColourId, initialColour);
+
+		button_attachment = std::make_unique<juce::AudioProcessorValueTreeState::ButtonAttachment>(
+				processor.getParameters(), buttonName, *this);
+	}
+
+	void clicked() override {
+		isToggled = getToggleState();
+	}
+
+	void paintButton(juce::Graphics& g, bool shouldDrawButtonAsHighlighted, bool shouldDrawButtonAsDown) override {
+		g.fillAll(isToggled ? toggleColour : initialColour);
+
+		auto drawable = juce::Drawable::createFromImageData(BinaryData::button_svg, BinaryData::button_svgSize);
+		if (drawable)
+			drawable->drawWithin(g, getLocalBounds().toFloat(), juce::RectanglePlacement::fillDestination, 1.0f);
+	}
+
+private:
+	juce::Colour initialColour;
+	juce::Colour toggleColour;
+	bool isToggled;
+	juce::String buttonName;
+	apcStepperMainProcessor& processor;
+	std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> button_attachment;
+};
