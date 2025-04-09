7517af4009d9c7fdad9614533a02ad3619c4fee4
Tom Peak Walcher <tompeakwalcher@Toms-MacBook-Pro.local>
2025-04-09 07:21:16 +0200

    custom font

--- Source/Plugin/RowToggle.h
+++ Source/Plugin/RowToggle.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "apcToggleButton.h"
+#include <juce_gui_basics/juce_gui_basics.h>
 #include <juce_gui_extra/juce_gui_extra.h>
 
 class RowToggle : public apcToggleButton {
@@ -11,6 +12,16 @@ public:
     RowToggle(juce::Colour initialColour, juce::Colour toggleColour)
        : apcToggleButton(initialColour, toggleColour){
         setButtonText("Velocity");
+        // Initialize SharedResourcePointer with a lambda that creates the Typeface
+        auto temp = juce::Typeface::createSystemTypefaceFor(BinaryData::ConthraxSemiBold_otf, BinaryData::ConthraxSemiBold_otfSize);
+        if (temp != nullptr)
+        {
+            typeface = juce::Typeface::Ptr(temp.get()); // Transfer ownership to Typeface::Ptr
+            customFont = juce::Font(typeface);
+            //customFont.setHeight(14.0f);;
+        }
+
+
     }
 
     void clicked() override {
@@ -27,9 +38,8 @@ public:
 
 
         // Draw the button text centered and scaled
-        g.setColour(findColour(juce::TextButton::textColourOnId));
-        g.setFont(juce::Font(getHeight() * 0.7f, juce::Font::bold)); // Scale font size
-
+        g.setColour(juce::Colours::darkgrey);
+        g.setFont(customFont); // Scale font size
         juce::Rectangle<float> textBounds = getLocalBounds().toFloat();
         g.drawFittedText(getButtonText(), getLocalBounds().reduced(5,0), juce::Justification::centred, 1); // Draw centered and scale if needed
 
@@ -37,6 +47,7 @@ public:
 
 private:
 
-
+    juce::Font customFont;
+    juce::Typeface::Ptr typeface;
     bool isToggled;
 };
